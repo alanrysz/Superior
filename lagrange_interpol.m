2fde81b83d2287659c7cea1c8b1478c782342ef2
alanrysz <alandvc@gmail.com>
2019-11-16 00:07:35 -0300

    Add files via upload

---
+++ lagrange_interpol.m
@@ -0,0 +1,15 @@
+function sum = lagrange_interpol(x,y)
+sum=0;
+    for i=1:length(x)
+        p=1;
+        for j=1:length(x)
+            if j~=i
+                c = poly(x(j))/(x(i)-x(j));
+                p = conv(p,c);
+            end
+        end
+        term = p*y(i);
+        sum= sum + term;
+    end
+    disp(sum);
+end
